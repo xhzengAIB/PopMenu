9d9fa87538dfd40e03215d1d9dee7bd53a302618
Jack <xhzengAIB@gmail.com>
2016-03-15 13:53:10 +0800

    修改描述

--- PopMenu/MenuButton.h
+++ PopMenu/MenuButton.h
@@ -3,7 +3,7 @@
 //  JackFastKit
 //
 //  Created by 曾 宪华 on 14-10-13.
-//  Copyright (c) 2014年 华捷 iOS软件开发工程师 曾宪华. All rights reserved.
+//  Copyright (c) 2014年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 // ==========================================
